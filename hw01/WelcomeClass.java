0b9ade4f42318d01babaa3b8d3c1e0e2432b7842
kew318 <kew318@lehigh.edu>
2015-01-28 00:23:52 +0000

    updated WelcomeClass

---
+++ hw01/WelcomeClass.java
@@ -0,0 +1,29 @@
+///////////////////////////////////////////////////////////////////////////////
+//Kelly Wu
+//Welcome Class Java Program
+//January 27, 2015
+//
+//  first compile the program
+//      javac WelcomeClass.java
+//  run the program
+//      java WelcomeClass//
+
+//  define a class
+public class WelcomeClass{
+
+//  add main method
+    public static void main(String[ ] args) {
+        
+//  print the statement with box around it
+    System.out.println("-----------");
+    System.out.println("| WELCOME |");
+    System.out.println("-----------");
+    System.out.println("^  ^  ^  ^  ^  ^");
+    System.out.println("/ \\/ \\/ \\/ \\/ \\/ \\ ");
+    System.out.println("<-E--J--K--0--0--0->");
+    System.out.println("\\ /\\ /\\ /\\ /\\ /\\ /");
+    System.out.println("v  v  v  v  v  v");
+    System.out.println("This August my wife and I took an eleven-day, 675-mile bike");
+    System.out.println("tour on our tandem. We circled Lake Ontario.");
+    }
+}
